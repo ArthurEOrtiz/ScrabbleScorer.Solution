427934ed4c13377f14ff9dd5257c2fa71900161a
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-06-28 21:44:19 -0600

    Testing
    Running into some errors during the build of our test.
    
    Co-Authored-By: Mauro Rosales Jr <97552823+maurorosalesjr@users.noreply.github.com>
    Co-Authored-By: Ryan Broadsword <88289822+rbroadsword@users.noreply.github.com>

--- ScrabbleScorer.Test/ModelTests/ScrabbleTest.cs
+++ ScrabbleScorer.Test/ModelTests/ScrabbleTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using ScrabbleScorer;
 
 namespace ScrabbleScorer.Tests
@@ -7,13 +8,28 @@ namespace ScrabbleScorer.Tests
   public class ScrabbleTests
   {
     [TestMethod]
-    public void StringSplit_TurnWordIntoArrayOfIndividualLetters_ArrayOfLetters()
-    {
-      Scrabble scrabbleWord = new Scrabble(); 
-      string singleWord = "dog";
-      char[] result = {"d","o","g"};
-      Assert.AreEqual(singleWord.SplitWord(), result )
-    }
+      public void LetterValue_GetLetterValueOfIndiviaualLetter_ValueOfLetters()
+      {
+        Scrabble scrabbleWord = new Scrabble(); 
+        Assert.AreEqual(1, scrabbleWord.LetterValues("A"), true);
+        
+      }
+  }  
+  //  NO =(
+    //Are you ok Alex? Do you need help?
+    
 
-  }
+
+    // public void StringSplit_TurnWordIntoArrayOfIndividualLetters_ArrayOfLetters()
+    // {
+    //   Scrabble scrabbleWord = new Scrabble(); 
+    //   string singleWord = "dog";
+    //   char[] result = {"d","o","g"};
+    //   Assert.AreEqual(singleWord.SplitWord(), result )
+    // }
+
+    // Console.WriteLine(letterValues);
+    // foreach (var item in letterValues)
+    // Console.WriteLine(item + ", " );
+
 }
--- ScrabbleScorer/Models/Scrabble.cs
+++ ScrabbleScorer/Models/Scrabble.cs
@@ -1,15 +1,31 @@
 using System.Collections.Generic;
+using System; 
 
 namespace ScrabbleScorer
 {
   public class Scrabble
   {
-    public static string SplitWord(string word)
+    //declare within a method 
+    public LetterValues(string userInput)
     {
-      string input = word;
-      string[] output = input.ToCharArray();
-      return output;
+      int charValue = 0;
+
+      Dictionary<char, int> letterValues = new Dictionary<char, int>()
+      {{"A", 1}, {"E", 1}, {"I", 1}, {"O", 1}, {"U", 1}, {"L", 1}, {"N", 1}, {"R", 1}, {"S", 1}, {"T", 1}, {"D", 2}, {"G", 2}, {"B", 3}, {"C", 3}, {"M", 3}, {"P", 3}, {"F", 4}, {"H", 4}, {"V", 4}, {"W", 4}, {"Y", 4}, {"K", 5}, {"J", 8}, {"X", 8}, {"Q", 10}, {"Z", 10}};
+
+      for (int index = 0; index < userInput.length; index++)
+      {
+        if (userInput.Conatins(letterValues[index]))
+        {
+          return charValue += letterValue[index]
+        }
+      }
+      
+      
+
+   
     }
+    
     //get word,
     //pass it through a loop to get the letters in an array. 
     //pass it though the a letter dictionary to get the point value of letter, 
